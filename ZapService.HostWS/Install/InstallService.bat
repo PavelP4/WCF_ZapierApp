6be63c60df842434f12fe076e028a9e649e8ea0a
PavelP4 <reginuser@gmail.com>
2014-12-24 14:19:49 +0300

    Add bat file to either install or uninstall service

---
+++ ZapService.HostWS/Install/InstallService.bat
@@ -0,0 +1,2 @@
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe %~dp0\ZapService.HostWS.exe
+rem %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /u %~dp0\ZapService.HostWS.exe
